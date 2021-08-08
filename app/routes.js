c3119ff41fde71e38499e8bc0c9cba974047270c
imperfect-geek <60563547+imperfect-geek@users.noreply.github.com>
2021-08-09 00:41:59 +0530

    first commit

--- app/routes.js
+++ app/routes.js
@@ -1,1 +1,66 @@
+const express = require("express");
+const passport = require("passport");
+const sendMail = require("./controller/sendMail");
 
+const app = express();
+const router = new express.Router();
+app.use(router);
+
+// @desc   Auth with google
+// @route  GET /auth/google
+router.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: [
+      "email",
+      "profile",
+      "https://mail.google.com/",
+      "https://www.googleapis.com/auth/gmail.modify",
+      "https://www.googleapis.com/auth/gmail.compose",
+    ],
+    accessType: "offline",
+    approvalPrompt: "force",
+  })
+);
+
+// @desc   Google Auth callback
+// @route  GET /auth/google/callback
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/auth/failure" }),
+  (req, res) => {
+    res.json({
+      code: 200,
+      status: "success",
+      message: "Google Authentication successful!",
+    });
+  }
+);
+
+// @desc   Failure redirect
+// @route  GET /auth/failure
+router.get("/auth/failure", (req, res) => {
+  res.json({
+    code: 400,
+    status: "failure",
+    message: "Google Authentication failed!",
+  });
+});
+
+// @desc   Logout route
+// @route  GET /logout
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.json({
+    code: 200,
+    status: "success",
+    message: "Logged Out!",
+  });
+});
+
+// @desc       Route for sending mails
+// @route      POST /sendmail
+// @req-body   name: String, subject: String, message: String
+router.post("/sendmail", sendMail);
+
+module.exports = router;
